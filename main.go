4e74aa9cc4087e2d70bc9b6ec9f0cabe9923d063
Henry Spivey <spivotron@gmail.com>
2019-11-12 21:10:35 -0800

    changing port

--- main.go
+++ main.go
@@ -8,11 +8,19 @@ import (
 	"survey-app/server/router"
 )
 
+func getPort() string {
+  p := os.Getenv("PORT")
+  if p != "" {
+    return ":" + p
+  }
+  return ":3000"
+}
+
 func main() {
 	r := router.Router()
 	// fs := http.FileServer(http.Dir("build"))
 	// http.Handle("/", fs)
 	fmt.Println("Starting server on the port 3001...")
-
-	log.Fatal(http.ListenAndServe(process.env.PORT, r))
+	port := getPort()
+	  log.Fatal(http.ListenAndServe(port, r))	
 }
