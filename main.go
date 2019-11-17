409e0f944d5922719e680279e0bb84290f5568d2
Henry Spivey <spivotron@gmail.com>
2019-11-17 11:12:42 -0800

    testing

--- main.go
+++ main.go
@@ -18,16 +18,13 @@ func getPort() string {
 
 func main() {
 	r := router.Router()	
+	buildHandler := http.FileServer(http.Dir("client/build"))
+	r.PathPrefix("/").Handler(buildHandler)
+
+
 
 	staticHandler := http.FileServer(http.Dir("client/build/static"))
 	r.PathPrefix("/static/").Handler(staticHandler)
-
-
-	buildHandler := http.FileServer(http.Dir("client/build"))
-	
-	r.PathPrefix("/").Handler(buildHandler)
-
-	
 	port := getPort()
 	log.Fatal(http.ListenAndServe(port, r))	
 }
