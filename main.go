0cb90f208967684d6b5ac8ab2034141f477521a8
Henry Spivey <spivotron@gmail.com>
2019-11-17 10:20:01 -0800

    testing

--- main.go
+++ main.go
@@ -18,7 +18,13 @@ func getPort() string {
 
 func main() {
 	r := router.Router()	
-	
+
+	buildHandler := http.FileServer(http.Dir("client/build"))
+	r.PathPrefix("/").Handler(buildHandler)
+	r.PathPrefix("/survey").Handler(buildHandler)
+	staticHandler := http.FileServer(http.Dir("client/build/static"))
+	r.PathPrefix("/static/").Handler(staticHandler)
+
 	port := getPort()
 	log.Fatal(http.ListenAndServe(port, r))	
 }
--- server/router/router.go
+++ server/router/router.go
@@ -3,18 +3,11 @@ package router
 import (
 	"survey-app/server/middleware"
 	"github.com/gorilla/mux"
-	"net/http"
 )
 
 // Router is exported and used in main.go
 func Router() *mux.Router {
 	router := mux.NewRouter()
-
-
-	buildHandler := http.FileServer(http.Dir("../client/build"))
-	router.PathPrefix("/").Handler(buildHandler)
-	staticHandler := http.FileServer(http.Dir("../client/build/static"))
-	router.PathPrefix("/static/").Handler(staticHandler)
 
 	router.HandleFunc("/api/survey", middleware.GetAllSurveys).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/survey/{id}", middleware.GetSurvey).Methods("GET", "OPTIONS")
