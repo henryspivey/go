11dc9fe1939daac0c51fd6384c85ad625faf8424
Henry Spivey <spivotron@gmail.com>
2019-11-17 11:29:10 -0800

    testing

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	r := router.Router()	
 
 	buildHandler := http.FileServer(http.Dir("client/build"))
-	r.PathPrefix("/survey/*").Handler(http.StripPrefix("/survey/*", buildHandler))
+	r.PathPrefix("/survey/").Handler(http.StripPrefix("/survey/", buildHandler))
 	r.PathPrefix("/").Handler(buildHandler)
 
 
