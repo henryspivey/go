c0e6e28d6ee1dcaf3532338d98a4e2c59d51bc27
Henry Spivey <spivotron@gmail.com>
2019-11-17 10:51:23 -0800

    testing

--- server/router/router.go
+++ server/router/router.go
@@ -10,7 +10,7 @@ func Router() *mux.Router {
 	router := mux.NewRouter()
 
 	router.PathPrefix("/survey").Subrouter()
-	router.HandleFunc("/survey/{id}", middleware.GetSurvey).Methods("GET", "OPTIONS")
+	router.HandleFunc("/survey", middleware.GetSurvey).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/survey", middleware.GetAllSurveys).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/survey/{id}", middleware.GetSurvey).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/survey", middleware.CreateSurvey).Methods("POST", "OPTIONS")		
